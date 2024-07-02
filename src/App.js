475d1d8ad43e3a29defff13fa0b93719133d065b
voidcodeinit <infinitetimehalo@gmail.com>
2024-07-02 16:28:54 +0330

    first commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+// import logo from './logo.svg';
+// import './App.css';
+import { Fragment, useState } from 'react';
+import Header from './components/Layout/Header';
+import Meals from './components/Meals/Meals';
+import Cart from './components/Cart/Cart';
+import CartProvider from './store/CartProvider';
+
+function App() {
+  const [cartIsShown, setCartIsShown] = useState(false);
+  const showCartHandler = () => setCartIsShown(true);
+  const hideCartHandler = () => setCartIsShown(false);
+
+  return (
+    <CartProvider>
+      {/* <Cart /> */}
+      {cartIsShown && <Cart onClose={hideCartHandler} />}
+      <Header onShowCart={showCartHandler} />
+      <main>
+        <Meals />
+      </main>
+    </CartProvider>
+  );
+}
+
+export default App;
